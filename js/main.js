fa965505975ad525e5188c54f0f90a379121aada
lilrooness <jfrangoudes@gmail.com>
2013-03-27 23:37:40 +0000

    UI Added
    If you mouseover the test image a user interface pops up with forward and back buttons, I need some propper buttons made my someone else because at the moment im working from my mac and it doesn't have photoshop

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-(function () {
-	Image img = new Image();
-	img.src = "";
-})();
+window.onload = (function () {
+	img = new Image();
+	img.src = "http://www.southpacifichotel.com.hk/images/photo-tour-pics/lu_lu_restaurant.jpg";
+	viewport = $("#main-viewer")[0];
+	ctx = viewport.getContext("2d");
+	ui = $("#ui");
+	ctx.drawImage(img, 0, 0, viewport.width, viewport.height);
+
+	$("#main-viewer").mouseover(function () {
+		console.log("in");
+		ui.fadeIn();
+	});
+
+	$("#main-viewer").mouseout(function () {
+		console.log("out");
+		ui.fadeOut();
+	});
+});
