3e97da9b0d4b4f2eb2cd8d80634bac043115040b
lilrooness <jfrangoudes@gmail.com>
2013-03-31 19:00:17 +0100

    Fade in/out effects now working

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 window.onload = (function () {
 	img = new Image();
 	img.src = "http://www.southpacifichotel.com.hk/images/photo-tour-pics/lu_lu_restaurant.jpg";
-	viewport = $("#main-viewer")[0];
-	ctx = viewport.getContext("2d");
+	viewport = $("#slide");
 	ui = $("#ui");
-	ctx.drawImage(img, 0, 0, viewport.width, viewport.height);
+	viewport.src = img.src;
+	console.log(viewport.src);
 
-	$("#main-viewer").mouseover(function () {
-		console.log("in");
-		ui.fadeIn();
-	});
-
-	$("#main-viewer").mouseout(function () {
-		console.log("out");
-		ui.fadeOut();
+	$("#wrapper").hover(function(){
+    	$("#ui").fadeIn("fast");
+	},
+	function(){
+		$("#ui").fadeOut("fast");
 	});
 });
