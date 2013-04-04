4a50bd306b97d38a84dfda948df1a0ee7c6de2a0
lilrooness <jfrangoudes@gmail.com>
2013-04-04 12:25:43 +0100

    Now able to cycle through images

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 window.onload = (function () {
-	img = new Image();
-	img.src = "http://www.southpacifichotel.com.hk/images/photo-tour-pics/lu_lu_restaurant.jpg";
-	viewport = $("#slide");
-	ui = $("#ui");
-	viewport.src = img.src;
-	console.log(viewport.src);
+    imageIndex = 0;
+    images = ["http://www.southpacifichotel.com.hk/images/photo-tour-pics/lu_lu_restaurant.jpg",
+        "http://i.dailymail.co.uk/i/pix/2012/11/15/article-0-1609D0FF000005DC-373_964x641.jpg"];
 
-	$("#wrapper").hover(function(){
-    	$("#ui").fadeIn("fast");
-	},
-	function(){
-		$("#ui").fadeOut("fast");
-	});
+    var update = function () {
+        imageIndex = imageIndex % images.length;
+        document.getElementById("slide").src = images[imageIndex];
+    }
+
+    update();
+
+    ui = $("#ui");
+
+    $("#wrapper").hover(function(){
+        $("#ui").fadeIn("fast");
+    },
+    function(){
+        $("#ui").fadeOut("fast");
+    });
+
+    document.getElementById("forward").onclick = function () {
+        imageIndex ++;
+        update();
+    };
+
+    document.getElementById("back").onclick = function () {
+        imageIndex --;
+        update();
+    };
 });
