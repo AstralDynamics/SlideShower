9da5c92fcc9a97298e87e9aa0391c494396a6ac1
eeue56 <eeue56@bangor.ac.uk>
2013-05-01 15:13:23 +0000

    fixing slideshower loading

--- PHP/get_slides.php
+++ PHP/get_slides.php
@@ -1,17 +1,11 @@
 <?php
 
-$hut_slides = array("hutSlides/slide1.png",
-  		"hutSlides/slide2.png",
-			"hutSlides/slide3.png",
-			"hutSlides/slide4.png",
-			"hutSlides/slide5.png",
-			"hutSlides/slide6.png");
+$hut_slides = array("slides/slide1.png",
+  		"slides/slide2.png",
+			"slides/slide3.png");
 $slide_times = array(1,
 				2,
-				5,
-				-1,
-				7,
-				1);
+				5);
 				
 $slides=array("slides"=>$hut_slides,
 		"times"=>$slide_times);
--- js/main.js
+++ js/main.js
@@ -10,25 +10,18 @@ SlideShower.initImages([
 */
 
 
-var slides = [];
-var times = [];
-//slideData = jQuery.parseJSON($.get("url/php/source.php", "empty", "json"));
-var slideData = $.parseJSON('{ \
-	"slides": [ \
-  				"http://i.animecrazy.net/Xros_Wars_lol.png",\
-  				"http://www.animeout.com/wp-content/uploads/2012/12/cowboy_bebop_desktop_1024x768_wallpaper-276818.jpg",\
-  				"http://images6.alphacoders.com/311/311018.jpg",\
-  				"http://cdn.myanimelist.net/images/characters/11/73929.jpg"\
-  				],\
-  	"times": [1000, 3000, 2000, 1500]\
-}');
-
-SlideShower.initImages(slideData["slides"], slideData["times"]);
-
-SlideShower.loadImages();
 
 
 $(document).ready(function () {    
+	loadSlide = function(){
+
+		data = $.ajax({url : "http://astraldynamics.co.uk/SlideShower/PHP/get_slides.php", async : false});
+		slideData = $.parseJSON(data.responseText);
+		SlideShower.initImages(slideData["slides"], slideData["times"]);
+		SlideShower.loadImages();
+	};
+	loadSlide();
+
     SlideShower.bindControls();
     SlideShower.displayImages();
 });
